463e16a90c7a39bd8b3a68299744d44d95398ae4
Amit Kumar Jha <Amitjha1412@gmail.com>
2014-05-06 02:46:14 +0530

    first commit

---
+++ ocr_gui.m
@@ -0,0 +1,201 @@
+function varargout = ocr_gui(varargin)
+% OCR_GUI MATLAB code for ocr_gui.fig
+%      OCR_GUI, by itself, creates a new OCR_GUI or raises the existing
+%      singleton*.
+%
+%      H = OCR_GUI returns the handle to a new OCR_GUI or the handle to
+%      the existing singleton*.
+%
+%      OCR_GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in OCR_GUI.M with the given input arguments.
+%
+%      OCR_GUI('Property','Value',...) creates a new OCR_GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before ocr_gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to ocr_gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help ocr_gui
+
+% Last Modified by GUIDE v2.5 15-Apr-2014 08:32:53
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ocr_gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @ocr_gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before ocr_gui is made visible.
+function ocr_gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to ocr_gui (see VARARGIN)
+
+% Choose default command line output for ocr_gui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+
+
+% UIWAIT makes ocr_gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = ocr_gui_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% --- Executes on button press in pb_load.
+function pb_load_Callback(hObject, eventdata, handles)
+% hObject    handle to pb_load (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[filename, pathname] = uigetfile({'*.jpg';'*.bmp';'*.gif';'*.tif'}, 'Pick an Image File');
+img=imread([pathname,filename]);
+set(handles.text1,'String',[filename]);
+set(handles.text1,'FontSize',14);
+axes(handles.img_display);
+imagesc(img);
+address = cat(2,pathname,filename);
+imagen=imread(address);
+% Show image
+imshow(imagen);
+% Convert to gray scale
+if size(imagen,3)==3 %RGB image
+    imagen=rgb2gray(imagen);
+end
+% Convert to BW
+threshold = graythresh(imagen);
+imagen =~im2bw(imagen,threshold);
+% Remove all object containing fewer than 30 pixels
+imagen = bwareaopen(imagen,30);
+%Storage matrix word from image
+word=[ ];
+text=[ ];
+re=imagen;
+% Load templates
+load templates
+global templates
+% Compute the number of letters in template file
+num_letras=size(templates,2);
+while 1
+    %Fcn 'lines' separate lines in text
+    [fl re]=lines(re);
+    imgn=fl;
+    %-----------------------------------------------------------------
+    % Label and count connected components
+    [L Ne] = bwlabel(imgn);
+    for n=1:Ne
+        [r,c] = find(L==n);
+        % Extract letter
+        n1=imgn(min(r):max(r),min(c):max(c));
+        % Resize letter (same size of template)
+        img_r=imresize(n1,[42 24]);
+        %-------------------------------------------------------------------
+        % Call fcn to convert image to text
+        letter=read_letter(img_r,num_letras);
+        % Letter concatenation
+        word=[word letter];
+    end
+    text = char(text, word);
+    % Clear 'word' variable
+    word=[ ];
+    %*When the sentences finish, breaks the loop
+    if isempty(re)  %See variable 're' in Fcn 'lines'
+        break
+    end
+end
+set(handles.img_display,'Visible','off');
+set(handles.text2,'String',[text]);
+set(handles.text2,'FontSize',24);
+guidata(hObject, handles);
+
+
+% --------------------------------------------------------------------
+function FileMenu_Callback(hObject, eventdata, handles)
+% hObject    handle to FileMenu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function OpenMenuItem_Callback(hObject, eventdata, handles)
+% hObject    handle to OpenMenuItem (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+file = uigetfile('*.fig');
+if ~isequal(file, 0)
+    open(file);
+end
+
+% --------------------------------------------------------------------
+function PrintMenuItem_Callback(hObject, eventdata, handles)
+% hObject    handle to PrintMenuItem (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+printdlg(handles.figure1)
+
+% --------------------------------------------------------------------
+function CloseMenuItem_Callback(hObject, eventdata, handles)
+% hObject    handle to CloseMenuItem (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+selection = questdlg(['Close ' get(handles.figure1,'Name') '?'],...
+                     ['Close ' get(handles.figure1,'Name') '...'],...
+                     'Yes','No','Yes');
+if strcmp(selection,'No')
+    return;
+end
+
+delete(handles.figure1)
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = get(hObject,'String') returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+     set(hObject,'BackgroundColor','white');
+end
